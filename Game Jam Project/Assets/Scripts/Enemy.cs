02107c26cba65dcbb3cb2adf9b3b309464c6d4b3
kandyelmo <43852025+kandyelmo@users.noreply.github.com>
2019-01-26 20:30:23 -0500

    Update Enemy.cs

--- Game Jam Project/Assets/Scripts/Enemy.cs
+++ Game Jam Project/Assets/Scripts/Enemy.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
-
+    public LayerMask enemyMask;
+    public float speed;
+    Rigidbody2D rb;
+    Transform tf;
+    float width;
 	// Use this for initialization
 	void Start () {
-		
+        tf = this.transform;
+        rb = this.GetComponent<Rigidbody2D>();
+        width = this.GetComponent<SpriteRenderer>().bounds.extents.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector2 lineCastPos = tf.position - tf.right * width;
+        
+        // always move foward
+        Vector2 myVel = rb.velocity;
+        myVel.x = speed;
+        rb.velocity = myVel;
 	}
 }
